34b8e7d569d7de6b096a684515ce30690aad3d0d
rahul-jha98 <jharahul1998@gmail.com>
2019-01-26 04:24:41 +0530

    Initial commit

---
+++ app/src/main/java/com/rahul/hope/HomeActivityFragment.kt
@@ -0,0 +1,20 @@
+package com.rahul.hope
+
+import android.support.v4.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+class HomeActivityFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
