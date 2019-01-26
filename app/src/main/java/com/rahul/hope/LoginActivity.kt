01d63daa3679e5dd52e9481f41c41ec8e717e06a
RaiAnant <RaiAnant.iit2017103@iiita.ac.in>
2019-01-26 11:44:27 +0530

    Login Activity

---
+++ app/src/main/java/com/rahul/hope/LoginActivity.kt
@@ -0,0 +1,87 @@
+package com.rahul.hope
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import java.util.*
+
+class LoginActivity : AppCompatActivity() {
+
+    private var mFirebaseAuth: FirebaseAuth? = null
+    private var mAuthStateListner: FirebaseAuth.AuthStateListener? = null
+    private var mUsername: String? = null
+    val ANONYMOUS = "anonymous"
+    private val RC_SIGN_IN = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        mUsername = ANONYMOUS
+
+        mFirebaseAuth = FirebaseAuth.getInstance()
+
+        mAuthStateListner = FirebaseAuth.AuthStateListener { firebaseAuth ->
+            val user = firebaseAuth.currentUser
+            if (user != null) {
+                Toast.makeText(this, "You are now signed in", Toast.LENGTH_SHORT).show()
+                onSignedInInitialize(user.displayName)
+            } else {
+                onSignedOutCleanup()
+                startActivityForResult(
+                    AuthUI.getInstance()
+                        .createSignInIntentBuilder().setIsSmartLockEnabled(false)
+                        .setAvailableProviders(
+                            Arrays.asList<AuthUI.IdpConfig>(
+                                AuthUI.IdpConfig.GoogleBuilder().build(),
+                                AuthUI.IdpConfig.EmailBuilder().build()
+                            )
+                        )
+                        .build(),
+                    RC_SIGN_IN
+                )
+            }
+        }
+    }
+
+    fun onSignedInInitialize(userName: String?) {
+        mUsername = userName
+    }
+
+    fun onSignedOutCleanup() {
+        mUsername = ANONYMOUS
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show()
+            } else if (resultCode == RESULT_CANCELED) {
+                finish()
+            }
+        }
+    }
+
+    protected override fun onResume() {
+        super.onResume()
+        if (mAuthStateListner != null) {
+            mFirebaseAuth!!.addAuthStateListener(mAuthStateListner!!)
+        }
+    }
+
+    protected override fun onPause() {
+        super.onPause()
+        if (mAuthStateListner != null) {
+            mFirebaseAuth!!.removeAuthStateListener(mAuthStateListner!!)
+        }
+    }
+}
